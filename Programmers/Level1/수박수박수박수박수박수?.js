b23047b6457343553695a88505764fb84c3c65e7
ehdgusdl9177 <k23129177@gmail.com>
2021-11-25 19:23:56 +0900

    평균구하기

--- Programmers/Level1/수박수박수박수박수박수?.js
+++ Programmers/Level1/수박수박수박수박수박수?.js
@@ -1,9 +1,12 @@
 function solution(n) {
   let answer = "";
   for (let i = 0; i < n; i++) {
+    // 만약 i 번째가 홀수 일때 "수"를 할당
     if (i % 2 === 1) {
       answer += "수";
-    } else if (i % 2 === 0) {
+    }
+    // 만약 i 번째가 짝수 일때 "박"을 할당
+    else if (i % 2 === 0) {
       answer += "박";
     }
   }
