94f09af6e92754aa1cf1a7cb32224dddb6040416
ehdgusdl9177 <k23129177@gmail.com>
2021-12-12 02:11:10 +0900

    나누어떨어지는숫자배열

---
+++ Programmers/Level1/나누어 떨러지는 숫자 배열.js
@@ -0,0 +1,9 @@
+function solution(arr, divisor) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result.length === 0 ? [-1] : result.sort((a, b) => a - b);
+}
