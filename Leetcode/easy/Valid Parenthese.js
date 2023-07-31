8fd5101051a196228fff42da6697641fa3128629
ehdgusdl9177 <k23129177@gmail.com>
2023-07-31 11:21:50 +0900

    Valid Parenthese

---
+++ Leetcode/easy/Valid Parenthese.js
@@ -0,0 +1,45 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else {
+      let p = stack.pop();
+      if (
+        (s[i] === ")" && p !== "(") ||
+        (s[i] === "}" && p !== "{") ||
+        (s[i] === "]" && p !== "[")
+      )
+        return false;
+    }
+  }
+  return stack.length === 0;
+};
