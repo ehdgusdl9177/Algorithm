9e49f5163b3f46b4d2766912c3da39b0c9763365
ehdgusdl9177 <k23129177@gmail.com>
2023-11-29 14:57:48 +0900

    윤달확인 알고리즘

--- Leetcode/easy/LeapYear.js
+++ Leetcode/easy/LeapYear.js
@@ -1,3 +1,18 @@
 /**
  * 윤달인지 아닌지 확인하는 알고리즘
  */
+const isLeap = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "Leap year.";
+      } else {
+        return "Not leap year.";
+      }
+    } else {
+      return "Leap year.";
+    }
+  } else {
+    return "Not leap year.";
+  }
+};
